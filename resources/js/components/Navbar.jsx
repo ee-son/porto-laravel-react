283dc7bdf6a05dc363c1968fa78912e4f4cac28d
ee-son <isan.faizun.hidar@mail.ugm.ac.id>
2025-07-10 11:54:51 +0700

    update navbar while scrolling

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="navbar navbar-expand-lg fixed-top bg-white">
+    <nav
+      className={`navbar navbar-expand-lg fixed-top ${
+        scrolled ? 'bg-inverse' : 'bg-white'
+      }`}
+    >
       <div className="container">
         <h4 className="ms-3">Isan FH</h4>
         <button
-          className="navbar-toggler"
+          className={`navbar-toggler ${scrolled ? 'navbar-dark' : 'navbar-light'}`}
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarToggleExternalContent"
@@ -16,12 +31,23 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarToggleExternalContent">
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarToggleExternalContent"
+        >
           <div className="navbar-nav me-3">
-            <a className="nav-link" href="#home">Home</a>
-            <a className="nav-link" href="#tech">Tech</a>
-            <a className="nav-link" href="#project">Project</a>
-            <a className="nav-link" href="#about">About</a>
+            <a className="nav-link" href="#home">
+              Home
+            </a>
+            <a className="nav-link" href="#tech">
+              Tech
+            </a>
+            <a className="nav-link" href="#project">
+              Project
+            </a>
+            <a className="nav-link" href="#about">
+              About
+            </a>
           </div>
         </div>
       </div>
