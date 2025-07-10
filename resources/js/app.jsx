571013436c84e13961770509dbae95fbf2f3d059
ee-son <isan.faizun.hidar@mail.ugm.ac.id>
2025-07-10 13:29:52 +0700

    bug fix: blank tech in cards

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../css/app.css';
+import '../scss/app.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
